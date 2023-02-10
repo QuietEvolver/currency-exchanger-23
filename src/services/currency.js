b9cfb5396b71b471aec5cf24343df131f2fae327
Vera Weikel <vera.weikel@gmail.com>
2023-02-10 09:01:19 -0800

    Adds CurrencyExchangeService api js file.

---
+++ src/services/currency.js
@@ -0,0 +1,17 @@
+export default class CurrencyExchangeService {  
+  static async getCurrency(rain) {
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${rain}&appid=${process.env.WEATHER_API_KEY}`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}
+        ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
+
