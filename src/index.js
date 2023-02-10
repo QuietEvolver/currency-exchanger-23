82989210d3213c67515df24adb9b1b4664c0492b
Justin Lee <lee.justin001126@gmail.com>
2023-02-09 16:36:31 -0800

    debugging api response Co-authored by: Molly Donegan <mollyrdonegan@gmail.com> Co-authored by: Vera Weikel <vera.weikel@gmail.com> Co-authored by: Jannon Sielaff<jannon.sielaff@gmail.com>

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ function getAPIData(city) {
         const errorMessage = `there was a problem accessing the ticket events data from Giphy API: ${ticketResponse.message}.`;
         throw new Error(errorMessage);
       }
-      const cityName = ticketResponse._embedded.events;//_embedded.venues.city.name
+      const cityName = ticketResponse._embedded.events[0].name;
+      console.log(cityName);
       displayTickets(cityName, city);
       return GifApiCall.getGif(cityName);
     })
