5b289bfd5adef6d92671aba1100852abf7697cbc
Vera Weikel <vera.weikel@gmail.com>
2023-02-11 21:45:40 -0800

    Update static api endpoint on currency.js

--- src/services/currency.js
+++ src/services/currency.js
@@ -1,7 +1,7 @@
 export default class CurrencyExchangeService {  
   static async getCurrency(usDollar) {
     try {
-      const response = await fetch(`https://api.simpleswap.io/get_currency?api_key=${process.env.API_KEY}&symbol=btcs`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}
