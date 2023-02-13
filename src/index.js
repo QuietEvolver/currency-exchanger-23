577bc63f008fb31b36b87443d2272e40f486e631
Vera Weikel <vera.weikel@gmail.com>
2023-02-12 23:32:54 -0800

    incorporating too many strategies; pulling to clear.

--- src/index.js
+++ src/index.js
@@ -18,59 +18,57 @@ function getAPIData(userInput) {
       printError(error);
     });
 } 
+// function getCurrency(){
+//   let responseDoc =  CurrencyExchangeService.getCurrency();
+//     responseDoc.then(function(rate){
+//       displayCurrencies(rate);
+//     }, function(error){
+//       printError(error);
+//     });
+//   }
 
 // currency fxn for latest rates
-function displayCurrencies(req, res){
-  const currentRates = req.conversion_rates;
-  // console.log("current Rates: ", currentRates);
+function displayCurrencies(req, res){ //
+  // const currentRates = req.conversion_rates;
+  // // console.log("current Rates: ", currentRates);
 
-  let inputUSDmult = req.conversion_rates["USD"] * res;
-  console.log("Outside for DispCurr(): jsonifiedResponse * resUSD: ", inputUSDmult);
-  for (const [key, value] of Object.entries(currentRates)) {
-    let usdMultiplier = req.conversion_rates["USD"] * res;
-    console.log("Inside for DispCurr(): jsonifiedResponse * resUSD: ", usdMultiplier);
-    console.log("${value}", value);
-    console.log("${key}", key);
-    console.log(`Display() ${key}: ${value}`);
-    // if (Object.keys(key==="USD")){
-    //   console.log(`pinned ${key==="USD"}: ${value}`);
-    // }
-    if (Object.keys(key==="AED")){
-      let ret =  value * usdMultiplier;
-      console.log(`pinned ${key==="AED"}: ${value}`);
-      console.log("ret", ret);
-      return ret;
-    }
-  }
-  console.log("current Rates[USD]: ", currentRates["USD"]);
-  console.log("res aka userInput: ", res);
-
-  document.querySelector("#currency").innerText += `\n Exchange rates in $${res} USD are equivalent to (radioSelect): ${req}`;
-}
-
-function printError(error) {
-  document.querySelector('#error').innerText = error;
-}
-
-function clearResults() {
-  document.querySelector("#currency").innerText = null;
-  document.querySelector('#error').innerText = null;
-}
-function formHandler(event) {
-  event.preventDefault();
-  clearResults();
+  // let inputUSDmult = req.conversion_rates["USD"] * res;
+  // console.log("Outside for DispCurr(): jsonifiedResponse * resUSD: ", inputUSDmult);
+  // for (const [key, value] of Object.entries(currentRates)) {
+  //   let usdMultiplier = req.conversion_rates["USD"] * res;
+  //   console.log("Inside for DispCurr(): jsonifiedResponse * resUSD: ", usdMultiplier);
+  //   console.log("${value}", value);
+  //   console.log("${key}", key);
+  //   console.log(`Display() ${key}: ${value}`);
+  //   // if (Object.keys(key==="USD")){
+  //   //   console.log(`pinned ${key==="USD"}: ${value}`);
+  //   // }
+  //   if (Object.keys(key==="AED")){
+  //     let ret =  value * usdMultiplier;
+  //     console.log(`pinned ${key==="AED"}: ${value}`);
+  //     console.log("ret", ret);
+  //     return ret;
+  //   }
+  // }
+  // console.log("current Rates[USD]: ", currentRates["USD"]);
+  // console.log("res aka userInput: ", res);
+  
   const usDollar = parseInt(document.querySelector('#user-input').value);
-  console.log("UserInput: usDollar: ", usDollar);
-  document.querySelector('#user-input').value = null;
-
-  let responseDoc = getAPIData(usDollar);
-
   const radioValue = document.querySelectorAll("input[name=rate]:checked");
+  // Getting just the conversion rates list from the response
+  const conversionRateList = CurrencyExchangeService.getCurrency().conversion_rates; // responseDoc
+  console.log('conversionRateList', conversionRateList);
+  console.log("UserInput: usDollar: ", usDollar);
   console.log("userSelection: ", radioValue);
 
-  // Getting just the conversion rates list from the response
-  const conversionRateList = responseDoc.conversion_rates;
-  console.log('conversionRateList', conversionRateList);
+  
+  document.querySelector("#currency").innerText += `\n Exchange rates in $${usDollar} USD are equivalent to (radioSelect):req ${req} res ${res}`;//;
+
+  document.querySelector('#user-input').value = null;
+
+  // // With await we are now getting the api response here. (errhandle)
+  // let responseDoc = await CurrencyExchangeService.getCurrency();
+  // // console.log('responseDoc', responseDoc);
 
   // Getting just the currencyRate from the country entered in the form
   const currencyRate = conversionRateList[radioValue];
@@ -84,90 +82,45 @@ function formHandler(event) {
   const roundedValue = Math.round((converted + Number.EPSILON) * 100) / 100;
   console.log('roundedValue', roundedValue);
 
-  for (const [key, value] of Object.entries(radioValue)) {
-    if(radioValue === key){
-      console.log(" key, value, radioValue", key, value, radioValue);
-      return value;
-    }
-    console.log(`${key}: ${value}`);
-  }
-  
-  // const form = document.querySelector("form");
-  // const log = document.querySelector("#log");
-
-  // const data = new FormData(form);
-  // //radio.rate  // Add each size
-  // // let value = this.rate; 
-  // for (const [key, value] of data) {
-  //   if (key === "size") {
-  //     rate = value;
+  // for (const [key, value] of Object.entries(radioValue)) {
+  //   if(radioValue === key){
+  //     console.log(" key, value, radioValue", key, value, radioValue);
+  //     return value;
   //   }
-  //   // let msg = "Choose an option";
-  //   switch(rate){
-  //   case("AED"): 
-  //     this.rate = "AED";
-  //     // ratePrice = currencyResponse.conversion_rates["AED"];
-  //     return ratePrice;
-  //   case("BAM"): 
-  //     this.rate = "BAM";
-  //     // ratePrice = currencyResponse.conversion_rates["BAM"];
-  //     return ratePrice;
-  //   case("CAD"):
-  //     this.rate = "CAD";
-  //     // ratePrice = currencyResponse.conversion_rates["CAD"];
-  //     return ratePrice;
-  //   case("DJF"): 
-  //     this.rate = "DJF";
-  //     // ratePrice = currencyResponse.conversion_rates["DJF"];
-  //     return ratePrice;
-  //   case("EGP"):
-  //     this.rate = "EGP";
-  //     // ratePrice = currencyResponse.conversion_rates["EGP"];
-  //     return ratePrice;
-  //   default: 
-  //     // TODO: Update log --> DOM to return a notification.
-  //     console.log("Choose a currency OR currency in question doesn't exist.");
-  //     // output.innerHTML = msg;  // webAPI: MessageChannel; 
-  //     console.log("entry(ies) ", output);
-  //     event.preventDefault();
-  //   }
-  //   log.innerText = output;
+  //   console.log(`${key}: ${value}`);
   // }
+}
 
-  // we update the name of the function that makes the API call
-  // findRate(usDollar);
-  getAPIData(usDollar);
+function printError(error) {
+  document.querySelector('#error').innerText = error;
+}
 
-  //   // console.log("incoming e", e);
-  // // e = SubmitEvent;
-  // // console.log("SubmitEvent e", e);
-  // const form = document.querySelector("form");
-  // const log = document.querySelector("#log");
-
-  // form.addEventListener(
-  //   "submit",
-  //   (event) => {
-  //     const data = new FormData(form);
-  //     let output = "";
-  //     for (const entry of data) { // FormDataEntryValue
-  //       output = `${output}${entry[0]}=${entry[1]}\r`;
-  //     }
-  //     log.innerText = output;
-  //     console.log("output: ", output);
-  //     console.log("e, form, log", event, form, log); 
-  //     event.preventDefault(); // }
-  //   },
-  //   false
-  // ); 
-
-  // // const formElement = document.getElementById("order-size");
-  const formUser = event.target;
+function clearResults() {
+  document.querySelector("#currency").innerText = null;
+  document.querySelector('#error').innerText = null;
+}
+function formHandler(event) {
+  event.preventDefault();
+  clearResults();
+  // const usDollar = parseInt(document.querySelector('#user-input').value);
+  // const radioValue = document.querySelectorAll("input[name=rate]:checked");
+  // // Getting just the conversion rates list from the response
+  // const conversionRateList = responseDoc.conversion_rates;
+  // console.log('conversionRateList', conversionRateList);
+  getAPIData();
   
-  const checkedInput = [...formUser.elements]
-    .filter((input) => input.checked) // Here you filter the inputs to get the checked value
-    .map((input) => input.checked); // here you get the checked input value
+  // console.log("UserInput: usDollar: ", usDollar);
+  // console.log("userSelection: ", radioValue);
+
+
   
-  console.log("checkedInput", checkedInput);
+  // const formUser = event.target;
+  
+  // const checkedInput = [...formUser.elements]
+  //   .filter((input) => input.checked) // Here you filter the inputs to get the checked value
+  //   .map((input) => input.checked); // here you get the checked input value
+  
+  // console.log("checkedInput", checkedInput);
 }
 
 window.addEventListener("load", function() {
