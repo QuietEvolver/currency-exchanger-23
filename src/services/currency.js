c5921190a79b66a97d4e3f4378906c10bea7f957
Vera Weikel <vera.weikel@gmail.com>
2023-02-13 00:25:58 -0800

    Deletes comments.

--- src/services/currency.js
+++ src/services/currency.js
@@ -1,5 +1,5 @@
 export default class CurrencyExchangeService {  
-  static async getCurrency() {//param
+  static async getCurrency() {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       const jsonifiedResponse = await response.json();
@@ -8,10 +8,6 @@ export default class CurrencyExchangeService {
         ${jsonifiedResponse.message}`;
         throw new Error(errorMessage);
       }
-      // console.log("param: ", param);
-      // console.log("jsonifiedResponse: ", jsonifiedResponse);
-      // console.log("jsonifiedResponse.conversion_rates: ", jsonifiedResponse.conversion_rates);
-      // console.log("jsonifiedResponse conversion rates in USD: ", jsonifiedResponse.conversion_rates["USD"]);
       return jsonifiedResponse;
     } catch(error) {
       return error;
